19c8fa4b8523655ad9c906fafb4132d7efce9c85
Miroslav Machacek <info@machsystems.cz>
2023-04-03 15:22:07 +0200

    TX delay compensation in can.c for CAN FD baud rates higher than 2 MBaud. CAN module clock increased to 80 MHz. STM32 H7 HAL updated.

--- CAN/Inc/can.h
+++ CAN/Inc/can.h
@@ -23,22 +23,31 @@
 #define MESSAGE_HEADER_SIZE_EXT_ID    7
 #define CONF_MESSAGE_SIZE_CAN         11
 
-#define CAN_OK                        0
-#define CAN_ERROR                     1
-#define CAN_CHANNEL_NOT_IMPLEMENTED   2
-#define CAN_WRONG_DLC                 3
-#define CAN_WRONG_PROTOCOL            4
-#define CAN_IS_NOT_RUNNING            5
-#define CAN_IS_ALREADY_RUNNING        6
-#define CAN_FIFO_FULL                 7
+#define CAN_OK                          0
+#define CAN_ERROR                       1
+#define CAN_CHANNEL_NOT_IMPLEMENTED     2
+#define CAN_WRONG_DLC                   3
+#define CAN_WRONG_PROTOCOL              4
+#define CAN_IS_NOT_RUNNING              5
+#define CAN_INCORRECT_PARAMETER         6
+#define CAN_FIFO_FULL                   7
+#define CAN_IS_ALREADY_RUNNING          8
+
+#define CORRECTION_CONST                80 //This is valid only for 40MHz CLK
+
+#define FDCAN_CLOCK                     80000000    // Peripheral has 80 MHz clock
 
 #define GREEN_BLINK_DURATION          100
 #define RED_BLINK_DURATION            300
+
+#define FDCAN_COMPENSATION_THRESHOLD    2000000 /* Delay compensation shall be used for Data baud rate above 2 Mbit */
 
 #define GREEN_CAN_LED1                0
 #define RED_CAN_LED1                  1
 #define GREEN_CAN_LED2                2
 #define RED_CAN_LED2                  3
+
+#define TDC_FILTER  0x0
 
 #include "stdbool.h"
 #include "main.h"
@@ -100,13 +109,14 @@ typedef struct
     uint8_t ArbitrationSJW;
     bool AutoStart;
     bool PreciseTimingSet;
-    uint8_t ArbitrationTimeSegment1;
+    uint16_t ArbitrationTimeSegment1;
     uint8_t ArbitrationTimeSegment2;
     uint8_t ArbitrationPrescaler;
     uint8_t DataTimeSegment1;
     uint8_t DataTimeSegment2;
     uint8_t DataPrescaler;
 } CanInitStruct;
+
 
 typedef struct
 {
@@ -173,8 +183,8 @@ uint8_t ToOutputMessageFormat(uint8_t *data, CanMessageStruct *message);
 /*
  *	Get can configuration values for specified baudrate.
  */
-uint8_t BaudToParametrs(uint8_t baudRate, uint8_t sPoint, uint8_t *prescaler,
-        uint8_t *tseg1, uint8_t *tseg2);
+uint8_t BaudToParametrs(uint8_t baudRate, uint8_t *pSPoint, uint8_t *prescaler,
+        uint16_t  *pTseg1, uint8_t *pTseg2, uint8_t dataBr);
 
 /*
  * Convert HAL message to standard message.
@@ -186,6 +196,8 @@ void RxMessageToFormat(FDCAN_RxHeaderTypeDef *RxHeader, uint8_t RxData[],
  *
  */
 uint8_t blinkLedCan(uint8_t Led);
+
+
 
 void CanLedTimerCallback(void);
 
